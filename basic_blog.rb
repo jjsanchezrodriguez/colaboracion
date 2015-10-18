7ea068b7a8cad5a7d496fbff35dfb379d4bf79c8
Jorge Sanchez <jjsanchezrodriguez@gmail.com>
2015-10-18 17:09:11 +0200

    Show sponsors post and page

--- basic_blog.rb
+++ basic_blog.rb
@@ -1,15 +1,27 @@
 require_relative "blog.rb"
 require_relative "post.rb"
 
-post1 = Post.new("Primer", "03/12/2015", "Este es el primer post y es muy bonito")
-post2 = Post.new("Segundo","04/12/2015", "Este es el segundo post y es muy bonitouy bonito", true)
-post3 = Post.new("Tercero","05/12/2015", "Este es el tercer post y es muy bonito bonito",true)
-post4 = Post.new("Cuarto", "06/12/2015", "Este es el cuarto post y es muy bonito bonito bonito")
-post5 = Post.new("Quinto", "07/12/2015", "Este es el quinto post y es muy bonito bonito bonito bonito")
-post6 = Post.new("Sexto",  "08/12/2015", "Este es el sexto post y es muy bonito bonito bonito bonito bonito",true)
+post1 = Post.new("Primero", "13/12/2015", "Este es el primer post y es muy bonito")
+post2 = Post.new("Segundo", "04/12/2015", "Este es el segundo post y es muy bonitouy bonito", true)
+post3 = Post.new("Tercero", "15/12/2015", "Este es el tercer post y es muy bonito bonito",true)
+post4 = Post.new("Cuarto",  "06/12/2015", "Este es el cuarto post y es muy bonito bonito bonito")
+post5 = Post.new("Quinto",  "07/12/2015", "Este es el quinto post y es muy bonito bonito bonito bonito")
+post6 = Post.new("Sexto",   "12/12/2015", "Este es el sexto post y es muy bonito bonito bonito bonito bonito",true)
+post7 = Post.new("Septimo", "13/12/2015", "Este es el septimo post y es muy bonito bonito bonito bonito bonito",true)
+post8 = Post.new("Octavo",  "14/12/2015", "Este es el octavo post y es muy bonito bonito bonito bonito bonito",true)
+post9 = Post.new("Noveno",  "14/12/2015", "Este es el octavo post y es muy bonito bonito bonito bonito bonito",true)
+post10 = Post.new("Decimo ",  "14/12/2015", "Este es el octavo post y es muy bonito bonito bonito bonito bonito",true)
+post11 = Post.new("Decimo Primero",  "14/12/2015", "Este es el octavo post y es muy bonito bonito bonito bonito bonito",true)
+post12 = Post.new("Decimo Segundo",  "14/12/2015", "Este es el octavo post y es muy bonito bonito bonito bonito bonito",true)
+post13 = Post.new("Decimo Tercero",  "14/12/2015", "Este es el octavo post y es muy bonito bonito bonito bonito bonito",true)
+post14 = Post.new("Decimo Cuarto",  "14/12/2015", "Este es el octavo post y es muy bonito bonito bonito bonito bonito",true)
+post15 = Post.new("Decimo Quinto",  "14/12/2015", "Este es el octavo post y es muy bonito bonito bonito bonito bonito",true)
+post16 = Post.new("Decimo Sexto",  "14/12/2015", "Este es el octavo post y es muy bonito bonito bonito bonito bonito",true)
+post17 = Post.new("Decimo Septimo",  "14/12/2015", "Este es el octavo post y es muy bonito bonito bonito bonito bonito",true)
 
-puts post1.show_title
-puts post1.show_date
+
+
+
 
 my_blog = Blog.new
 
@@ -19,5 +31,16 @@ my_blog.add_post(post3)
 my_blog.add_post(post4)
 my_blog.add_post(post5)
 my_blog.add_post(post6)
+my_blog.add_post(post7)
+my_blog.add_post(post8)
+my_blog.add_post(post9)
+my_blog.add_post(post10)
+my_blog.add_post(post11)
+my_blog.add_post(post12)
+my_blog.add_post(post13)
+my_blog.add_post(post14)
+my_blog.add_post(post15)
+my_blog.add_post(post16)
+my_blog.add_post(post17)
 
 my_blog.publish_front_page_bonus
--- blog.rb
+++ blog.rb
@@ -1,6 +1,6 @@
 class Blog
 	def initialize(array_post = [])
-		@array_post = array_post.sort! { |a, b|  a.date <=> b.date }
+		@array_post = array_post
 	end
 
 	def add_post(post)
@@ -28,34 +28,66 @@ class Blog
 	end
 
 	def publish_front_page_bonus
+
 		
-		page = 4
 
-		pos = @array_post.size / page
-		show_block_bonus(pos) 
+		pos = @array_post.size / 3
+		if @array_post.size % 3 > 0
+			pos += 1
+		end
+			
+		show = 1
+		
+		begin
+			show_block_bonus((show*3)-2)
+	
+			puts
 
+			pos.times do |i|
+	    		print "#{i+1}   "	
+			end
 
+			puts
+			puts
+			puts "> next"
+			show = gets.to_i 
+			puts "Bye !!!!!!!!!!!"
+		end while show >= 1 && show <= pos
 
 	end
 
-	def show_block_bonus(pos,page=4) #page allow chose the number of post for page 
-		(pos..(pos+page)).each do |i|
-			if(@array_post[i].show_sponsor == true)
-				puts "Post #{@array_post[i].show_title}"
+	def show_block_bonus(pos,page=2) #page allow chose the number of post for page 
+		
+		system("clear")
+		puts
+		puts "                                               THE AMAZING IRONHACK BLOG"
+		puts "                                             --------------------------------"
+		puts
+
+		pos -= 1
+		limite = pos + 3
+		while pos < limite && pos < @array_post.size
+			if @array_post[pos].show_sponsor == true
+				(@array_post[pos].show_title.size + 39).times {print "*"}
+				puts
+				puts "************** Post #{@array_post[pos].show_title} ******************"
+				(@array_post[pos].show_title.size + 39).times {print "*"}
 			else
-				puts "************ Post #{@array_post[pos].show_title} *******************"	
+				puts "Post #{@array_post[pos].show_title}"
+				(@array_post[pos].show_title.size + 6).times {print "*"}	
 			end
 
-			(@array_post[i].show_title.size + 6).times {print "*"}
 			puts
 
-
-			puts "Titulo #{@array_post[i].show_text}"
-			(@array_post[i].show_text.size + 7).times {print "-"}
+			puts "Titulo #{@array_post[pos].show_text}"
+			(@array_post[pos].show_text.size + 7).times {print "-"}
 			puts
+			puts
+
+			pos += 1
 		end
 	end
 
-	
+
 	
 end
